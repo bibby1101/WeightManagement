d4976fcc07dea5e63ec858b3f8697d228ce56ebc
bibby1101 <bibby1101@gmail.com>
2021-11-16 17:19:53 +0800

    open soft keyboard programmatically

--- app/src/main/java/com/evermore/fatman/MainActivity.kt
+++ app/src/main/java/com/evermore/fatman/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.evermore.fatman
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.TextView
 import com.evermore.fatman.databinding.ActivityMainBinding
 
@@ -16,5 +21,24 @@ class MainActivity : AppCompatActivity() {
 
         binding.info.text = getString(R.string.weight_title)
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                binding.weight.requestFocus()
+                binding.weight.showSoftKeyboard()
+            },
+            (0.5*1000).toLong()
+        )
+
+    }
+
+    // extension function to open soft keyboard programmatically
+    fun EditText.showSoftKeyboard(){
+        val imm = this.context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
     }
 }
